a60e3d19e0767b613b73f8c36a5059cad9b56495
Jakub Suder <jakub.suder@gmail.com>
2009-03-21 20:41:56 +0100

    added NSData extensions for Base64 encoding from CocoaDev.com

---
+++ Classes/NSDataMBBase64.h
@@ -0,0 +1,12 @@
+//
+//  NSDataMBBase64.h
+//  XBlip
+//
+// Downloaded from http://www.cocoadev.com/index.pl?BaseSixtyFour
+// Created by MiloBird - http://www.milobird.com/
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MBBase64)
+- (NSString *) base64Encoding;
+@end
